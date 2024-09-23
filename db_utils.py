8a3ca95e68de8a8d0fd6426e59686cdc76b323a4
Yasmine_BJ <107438126+ya-smin20@users.noreply.github.com>
2024-09-23 20:25:57 +0200

    Update db_utils.py

--- db_utils.py
+++ db_utils.py
@@ -4,9 +4,9 @@ def create_tables(db_url):
     conn = sqlite3.connect(db_url.split('///')[-1])
     cursor = conn.cursor()
     
-    # Check existing tables
+   
     cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    print(f"Existing tables: {cursor.fetchall()}")  # Debug print
+    print(f"Existing tables: {cursor.fetchall()}") 
     
     with open('database/create_tables.sql', 'r') as sql_file:
         sql_script = sql_file.read()
